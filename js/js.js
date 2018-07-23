af97a670c4b46b34774d022619fe6a722aca51bb
elvinlkw <elvinlkw@hotmail.com>
2018-07-23 17:33:27 -0400

    added js folder and added simple animation

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function animateText(){
+    var typed2 = new Typed('#typing-animation', {
+        strings: ['Welcome to my personal website . . .', 'It is not great, but not bad'],
+        typeSpeed: 30,
+        backSpeed: 30,
+        fadeOut: true,
+        loop: true
+    });
+}
+
+animateText();
