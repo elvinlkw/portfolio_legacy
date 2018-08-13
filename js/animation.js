a4f042d3cefe78b651f0a2a277bb8ccff16a93d1
elvinlkw <elvinlkw@hotmail.com>
2018-08-13 17:34:19 -0400

    added functionality to skills section

--- js/animation.js
+++ js/animation.js
@@ -1,36 +1,51 @@
-var arrow = $(".arrow-right");
+
 var settings = $("#navy");
 
-arrow.click(function(){
-    $(this).toggleClass("rotated");
-    
-    $("#hon").css("display", "block");
-});
-
+//menubar
 settings.hover(function(){
     $("#navbar").css("display", "block");
     }, function(){
         $("#navbar").css("display", "none");
 });
 
-function animateText(){
-    var typed2 = new Typed('#typing-animation', {
-        strings: ['Welcome to my mini portfolio website . . . ^1000', 'It is not great, but not bad'],
-        typeSpeed: 30,
-        backSpeed: 30,
-        smartBackspace: true,
-        loop: true
-    });
-}
 
+//arrow
+var arrow = $(".arrow-right");
+
+arrow.click(function(){
+
+    if(bToggle.is(':checked')){
+
+    }else{
+        $(this).toggleClass("rotated");
+
+        $(".arrow-content").slideToggle();    
+    }
+
+});
+
+//Skills Section Toggle Button
+
+var bToggle = $(".toggle-button");
+
+bToggle.click(function() { 
+    $(".arrow-content").slideToggle();
+    if(bToggle.not(':checked')){
+        $(arrow).toggleClass("rotated");
+    }
+
+})
+
+var typed2 = new Typed('#typing-animation', {
+    strings: ['Welcome to my mini portfolio website . . . ^1000', 'It is not great, but not bad'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    smartBackspace: true,
+    loop: true
+});
 
 var typed3 = new Typed('#about-me-animation', {
     strings: ['Hi, I\'m Elvin . . .^5000  and this is my story! ^2000'],
     typeSpeed: 30,
     loop:true
 });     
-
-
-
-
-animateText();
