4563b84fe0fa76e47ec6ada3c5f120549b499e87
elvinlkw <elvinlkw@hotmail.com>
2018-08-14 16:14:50 -0400

    added contact section

--- js/animation.js
+++ js/animation.js
@@ -50,7 +50,13 @@ bToggle.click(function() {
         $(arrow).toggleClass("rotated");
     }
 
-})
+});
+
+var menu_output = $(".menu-click");
+
+menu_output.click(function(){
+    $("#navbar").slideUp('slow');
+});
 
 var typed2 = new Typed('#typing-animation', {
     strings: ['Welcome to my mini portfolio website . . . ^1000', 'It is not great, but not bad'],
@@ -65,3 +71,43 @@ var typed3 = new Typed('#about-me-animation', {
     typeSpeed: 30,
     loop:true
 });     
+
+
+//Smooth-Scrolling Code
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
